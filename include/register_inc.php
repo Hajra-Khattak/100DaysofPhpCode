cb08b445448b6ead3ab5b0b547be554aaaed1985
Hajra-Khattak <129723906+Hajra-Khattak@users.noreply.github.com>
2024-10-11 22:41:25 +0500

    fifth day

--- include/register_inc.php
+++ include/register_inc.php
@@ -9,6 +9,22 @@ if (isset($_POST['submit'])){
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirmpassword = $_POST['confirmpassword'];
+
+    if( empty($username) || empty($mobile) || empty($email) || empty($password) || empty($confirmpassword)){
+        header("Location: ../register.php?error=emptyfields&username=".$username);
+        exit();
+    }
+    elseif(!preg_match("/^[a-zA-Z0-9]*/", $username)){
+        header("Location: ../register.php?error=invalidusername&username=".$username);
+        exit();
+    }
+    elseif($password !== $confirmpassword){
+        header("Location: ../register.php?error=passwordsdonotmatch&username=".$username);
+        exit();
+    }
+    else{
+        $sql = "";
+    }
 }
 
 ?>
